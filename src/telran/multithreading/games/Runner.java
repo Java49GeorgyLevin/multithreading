85c8bb1413dca7150fed9a1f09c95a5d06435340
Georgy Levin <g.phyzy@gmail.com>
2023-09-12 21:58:53 +0300

    HW 45: Race with synchronized

--- src/telran/multithreading/games/Runner.java
+++ src/telran/multithreading/games/Runner.java
@@ -3,10 +3,36 @@ package telran.multithreading.games;
 public class Runner extends Thread {
 private Race race;
 private int runnerId;
+private long runnerTime;
+private int runnerPlace;
+
+public void setRunnerTime(long runnerTime) {
+	this.runnerTime = runnerTime;
+}
+
+public long getRunnerTime() {
+	return runnerTime;
+}
+
+public void setRunnerPlace(int runnerPlace) {
+	this.runnerPlace = runnerPlace;
+}
+
+public long getRunnerPlace() {
+	return runnerPlace;
+}
+
+
 public Runner(Race race, int runnerId) {
 	this.race = race;
 	this.runnerId = runnerId;
 }
+
+
+public int getRunnerId() {
+	return runnerId;
+}
+
 @Override
 public void run() {
 	int sleepRange = race.getMaxSleep() - race.getMinSleep() + 1;
@@ -14,12 +40,16 @@ public void run() {
 	int distance = race.getDistance();
 	for (int i = 0; i < distance; i++) {
 		try {
-			sleep((long) (minSleep + Math.random() * sleepRange));
+			long loop = (long) (minSleep + Math.random() * sleepRange);
+			sleep(loop);
 		} catch (InterruptedException e) {
 			throw new IllegalStateException();
 		}
 		System.out.println(runnerId);
 	}
 	race.setWinner(runnerId);
+	race.setPlace(this);
 }
+
+
 }
