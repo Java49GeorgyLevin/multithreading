4b863522460abd7997856cbc36248b7a57da94cd
Georgy Levin <g.phyzy@gmail.com>
2023-09-25 15:55:54 +0300

    HW 46: winner atomic solution

--- src/telran/multithreading/games/Race.java
+++ src/telran/multithreading/games/Race.java
@@ -6,18 +6,16 @@ public class Race {
 	private int distance;
 	private int minSleep;
 	private int maxSleep;
-	private AtomicInteger winner = new AtomicInteger(-1);
+	AtomicInteger winner = new AtomicInteger(-1);
 	public Race(int distance, int minSleep, int maxSleep) {
 		this.distance = distance;
 		this.minSleep = minSleep;
 		this.maxSleep = maxSleep;
 	}
-	public AtomicInteger getWinner() {
-		return winner;
+	public int getWinner() {
+		return winner.get();
 	}
-	public void setWinner(int winner) {
-		this.winner.compareAndExchange(-1, winner);
-	}
+
 	public int getDistance() {
 		return distance;
 	}
