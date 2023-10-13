984146063e6588e34700df030c3416fa68d2369b
Georgy Levin <g.phyzy@gmail.com>
2023-10-13 18:35:14 +0300

    HW #49 updates List with LinkedList

--- src/telran/multithreading/messaging/MyLinkedBlockingQueue.java
+++ src/telran/multithreading/messaging/MyLinkedBlockingQueue.java
@@ -1,17 +1,15 @@
 package telran.multithreading.messaging;
 
+import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.LinkedList;
 import java.util.concurrent.locks.*;
 
 public class MyLinkedBlockingQueue<E> implements MyBlockingQueue<E> {
 	int limit;
-	
 	private LinkedList<E> queue = new LinkedList<>();
 	private Lock monitor = new ReentrantLock();
-	private Condition conditionForProducer = monitor.newCondition();
-	private Condition conditionForConsumer = monitor.newCondition();
-	
+	private Condition consumerWaitingCondition = monitor.newCondition();
+	private Condition producerWaitingCondition = monitor.newCondition();
 
 	public MyLinkedBlockingQueue(int limit) {
 		super();
@@ -22,11 +20,11 @@ public class MyLinkedBlockingQueue<E> implements MyBlockingQueue<E> {
 	public boolean add(E e) {
 		try {
 			monitor.lock();
-			if(queue.size() == limit) {
+			if (queue.size() == limit) {
 				throw new IllegalStateException();
 			}
 			boolean res = queue.add(e);
-			conditionForConsumer.signal();
+			consumerWaitingCondition.signal();
 			return res;
 		} finally {
 			monitor.unlock();
@@ -35,46 +33,51 @@ public class MyLinkedBlockingQueue<E> implements MyBlockingQueue<E> {
 
 	@Override
 	public boolean offer(E e) {
-		boolean res = false;
+		boolean res = true;
 		try {
 			monitor.lock();
-			if(queue.size() < limit) {
-				res = queue.add(e);
-				conditionForConsumer.signal();
-			}	
+			if (queue.size() == limit) {
+				res = false;
+			} else {
+				queue.add(e);
+				consumerWaitingCondition.signal();
+			}
+
 			return res;
 		} finally {
 			monitor.unlock();
-		}		
+		}
 	}
 
 	@Override
 	public void put(E e) throws InterruptedException {
 		try {
 			monitor.lock();
-			while(queue.size() == limit) {				
-					conditionForProducer.await();
+			while (queue.size() == limit) {
+				producerWaitingCondition.await();
 			}
 			queue.add(e);
-			conditionForConsumer.signal();			
+			consumerWaitingCondition.signal();
+
 		} finally {
 			monitor.unlock();
-		}		
+		}
+
 	}
 
 	@Override
 	public boolean offer(E e, long timeout, TimeUnit unit) throws InterruptedException {
 		try {
 			monitor.lock();
-			while(queue.size() == limit) {	
-				if(!conditionForProducer.await(timeout, unit)) {
+			while (queue.size() == limit) {
+				if (!producerWaitingCondition.await(timeout, unit)) {
 					return false;
-			} 
 				}
-				queue.add(e);
-				conditionForConsumer.signal();
-				return true;
-					
+			}
+			queue.add(e);
+			consumerWaitingCondition.signal();
+			return true;
+
 		} finally {
 			monitor.unlock();
 		}
@@ -84,56 +87,61 @@ public class MyLinkedBlockingQueue<E> implements MyBlockingQueue<E> {
 	public E take() throws InterruptedException {
 		try {
 			monitor.lock();
-			while( queue.isEmpty()) {
-				conditionForConsumer.await();
+			while (queue.isEmpty()) {
+				consumerWaitingCondition.await();
 			}
 			E res = queue.remove(0);
-			conditionForProducer.signal();
+			producerWaitingCondition.signal();
 			return res;
+
 		} finally {
 			monitor.unlock();
-		}		
+		}
 	}
 
 	@Override
 	public E poll(long timeout, TimeUnit unit) throws InterruptedException {
 		try {
 			monitor.lock();
-			while(queue.isEmpty()) {
-				if(!conditionForConsumer.await(timeout, unit)) {
+			while (queue.isEmpty()) {
+				if (!consumerWaitingCondition.await(timeout, unit)) {
 					return null;
 				}
 			}
 			E res = queue.remove();
+			producerWaitingCondition.signal();
 			return res;
-		} finally {
+
+		} finally
+
+		{
 			monitor.unlock();
 		}
-		
 	}
 
 	@Override
 	public E remove() {
-		try { 
+		try {
 			monitor.lock();
-			E res = queue.remove();
-			conditionForProducer.signal();;
-			return res;
-
+			E result = queue.remove();
+			producerWaitingCondition.signal();
+			return result;
 		} finally {
 			monitor.unlock();
-		}		
+		}
 	}
 
 	@Override
 	public E peek() {
-		E res = null;
+		E result = null;
 		try {
 			monitor.lock();
-			if(queue.size() != 0) {
-				res = queue.get(0);
+			if (queue.size() != 0) {
+				result = queue.get(0);
+
 			}
-			return res;
+
+			return result;
 		} finally {
 			monitor.unlock();
 		}
@@ -146,22 +154,24 @@ public class MyLinkedBlockingQueue<E> implements MyBlockingQueue<E> {
 			return queue.element();
 		} finally {
 			monitor.unlock();
-		}		
+		}
 	}
 
 	@Override
 	public E poll() {
-		E res = null;
+		E result = null;
 		try {
 			monitor.lock();
-			res = queue.poll();
-			if(res != null) {
-				conditionForProducer.signal();
+
+			result = queue.poll();
+			if (result != null) {
+				producerWaitingCondition.signal();
 			}
-			return res;			
+
+			return result;
 		} finally {
 			monitor.unlock();
-		}		
-	}	
+		}
+	}
 
 }
